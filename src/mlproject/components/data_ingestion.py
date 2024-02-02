1ead5fcc43941d86c45c7de461af76fbed84f344
JAGDISH CHAVAN <54933866+JagdishChavan081@users.noreply.github.com>
2024-02-03 00:43:38 +0530

    data ingestion

---
+++ src/mlproject/components/data_ingestion.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.joins("artifacts", "train.csv")
+    test_data_path: str = os.path.joins("artifacts", "test.csv")
+    raw_data_path: str = os.path.joins("artifacts", "raw.csv")
